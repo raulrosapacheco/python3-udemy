02a368bf8253622054ad5e25e871a2a80f41338b
Raul Rosa Pacheco <rr0608@outlook.com>
2021-09-27 21:19:51 -0300

    programming logic 27 set. 2021

--- programming-logic/for-in-range.py
+++ programming-logic/for-in-range.py
@@ -13,3 +13,16 @@ print('###########')
 
 for num in range(1, 10, 2):
     print(num)
+
+print('###########')
+
+list1 = ['Luiz Otavio', 'aJoao', 'Maria']
+
+for element in list1:
+    if element.lower().startswith('j'):
+        print('there is a word that starts with J')
+        break
+else:
+    print('there isn\'t  a word that starts with J')
+
+
